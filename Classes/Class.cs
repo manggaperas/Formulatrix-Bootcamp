7690bb59e1552c24c18050c748fd7617fbd027f4
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-05-25 09:00:02 +0700

    separate file class and program

---
+++ Classes/Class.cs
@@ -0,0 +1,28 @@
+namespace Classes;
+
+public class Animal
+{
+	public string Name;
+	public int Age;
+	public int Weight;
+	public int Height;
+	public bool Gender;
+
+	public Animal(string name, int age, int weight, int height, bool gender)
+	{
+		this.Name = name;
+		this.Age = age;
+		this.Weight = weight;
+		this.Height = height;
+		this.Gender = gender;
+	}
+
+	public void AnimalInfo()
+	{
+		Console.WriteLine("Name: {0}", this.Name);
+		Console.WriteLine("Age: {0}", this.Age);
+		Console.WriteLine("Weight: {0}", this.Weight);
+		Console.WriteLine("Height: {0}", this.Height);
+		Console.WriteLine("Gender: {0}", (this.Gender ? "Male" : "Female"));
+	}
+}
