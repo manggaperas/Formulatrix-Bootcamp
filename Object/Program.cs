7b0577b6061ec1d903a0bc9d8b3f8aa592d469e9
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-05-29 14:37:42 +0700

    my correction to array example on guide

---
+++ Object/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+
+object[] a1 = { "string", 123, true };
+object[] a2 = { "string", 123, true };
+Console.WriteLine(a1 == a2);
+Console.WriteLine(a1.Equals(a2));
+IStructuralEquatable se1 = (IStructuralEquatable)a1;
+Console.WriteLine(se1.Equals(a2, StructuralComparisons.StructuralEqualityComparer));
