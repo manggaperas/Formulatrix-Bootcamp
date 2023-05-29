ef7084cf127e6902f5a285076e3b037dd672c47f
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-05-29 15:09:28 +0700

    create factorial for practicing

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,13 @@
+/* Soal: Menghitung Faktorial */
+
+// Tulislah sebuah program dalam bahasa C# untuk menghitung faktorial dari sebuah bilangan bulat yang diberikan.
+// Faktorial dari sebuah bilangan n (dinyatakan sebagai n!) adalah hasil perkalian semua bilangan bulat positif
+// dari 1 hingga n.
+
+namespace FactorialLibrary;
+
+public class Factorial
+{
+	public int bilangan { get; set; }
+	
+}
---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FactorialLibrary;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Masukkan bilangan bulat positif: ");
+		int n = int.Parse(Console.ReadLine());
+
+		int faktorial = HitungFaktorial(n);
+
+		Console.WriteLine("Faktorial dari {0} adalah: {1}", n, faktorial);
+		Console.ReadLine();
+	}
+
+	static int HitungFaktorial(int n)
+	{
+		if (n == 0)
+			return 1;
+		else
+			return n * HitungFaktorial(n - 1);
+	}
+}
