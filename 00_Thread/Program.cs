ac4560d94e24bffccd19d5273183f2281c52f5c6
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-06-09 14:17:30 +0700

    submit thread practice

--- 00_Thread/Program.cs
+++ 00_Thread/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+static class Program
+{
+    static void Main()
+    {
+        Thread Thread1 = new Thread(PrintNumbers);
+        Thread Thread2 = new Thread(PrintNumbers);
+        Thread1.Name = "Thread1";
+        Thread2.Name = "Thread2";
+
+        Thread1.Start();
+        Thread2.Start();
+        Thread1.Join();
+        Thread2.Join();
+    }
+
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {i}");
+            Thread.Sleep(1000);
+        }
+    }
+}
