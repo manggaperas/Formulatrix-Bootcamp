a46abf2bc46784f40e12fc24393b6afa0b36fa64
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-06-09 15:29:00 +0700

    submit run task practice

--- 05_RunTask/Program.cs
+++ 05_RunTask/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+	static void Main()
+	{
+		Task task = new Task(() => Console.WriteLine("Hello, Task Start!"));
+		task.Start();
+		task.Wait(); // Wait for the task to complete
+
+		Task task1 = Task.Run(() => Console.WriteLine("Hello, Task.Run!"));
+		task1.Wait(); // Wait for the task to complete
+		
+		Task.Run(() => Console.WriteLine("Hello, Task Run Immediately!")); // Same as above
+	}
+}
