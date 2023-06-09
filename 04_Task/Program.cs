6330f116bcf16ffcf4a20a19d5022a5be8e6262e
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-06-09 15:29:28 +0700

    submit task practice

--- 04_Task/Program.cs
+++ 04_Task/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Task task = new Task(() => Console.WriteLine("Hello, Task!"));
+        task.Start();
+
+        task.Wait(); // Wait for the task to complete
+    }
+}
