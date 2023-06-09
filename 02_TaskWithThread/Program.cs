03711d9fbf642f6c0575bd7b08f865ae16a6059d
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-06-09 14:49:13 +0700

    submit task with thread practice

--- 02_TaskWithThread/Program.cs
+++ 02_TaskWithThread/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+public static class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Starting program.");
+		var stopwatch = new Stopwatch();
+		stopwatch.Start();
+
+		var thread1 = new Thread(DoTaskOne);
+		var thread2 = new Thread(DoTaskTwo);
+
+		thread1.Start();
+		thread2.Start();
+
+		thread1.Join();
+		thread2.Join();
+
+		stopwatch.Stop();
+
+		Console.WriteLine($"Program complete. Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+	}
+
+	public static void DoTaskOne()
+	{
+		Console.WriteLine("Starting task 1.");
+		StringBuilder x = new StringBuilder("Hello");
+		x.Append("Mangga");
+		for (int i = 0; i < 100000; i++)
+		{
+			x.Append(i).Append(" ");
+		}
+		Console.WriteLine("Task 1 complete.");
+	}
+
+	public static void DoTaskTwo()
+	{
+		Console.WriteLine("Starting task 2.");
+		StringBuilder y = new StringBuilder("Hello");
+		y.Append("Jeruk");
+		for (int i = 0; i < 100000; i++)
+		{
+			y.Append(i).Append(" ");
+		}
+		Console.WriteLine("Task 2 complete.");
+	}
+}
