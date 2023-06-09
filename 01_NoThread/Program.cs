8abca88f9d0e969e378d75946f2d32411759fcf3
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-06-09 14:40:39 +0700

    submit no thread practice

--- 01_NoThread/Program.cs
+++ 01_NoThread/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Starting program.");
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        DoTaskOne();
+        DoTaskTwo();
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Program complete. Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+    }
+
+    public static void DoTaskOne()
+    {
+        Console.WriteLine("Starting task 1.");
+        StringBuilder x = new StringBuilder("Hello");
+        x.AppendLine("Mangga");
+        for (int i = 0; i < 1000000; i++)
+        {
+            x.Append(i).Append(" ");
+        }
+        Console.WriteLine("Task 1 complete.");
+    }
+
+
+    public static void DoTaskTwo()
+    {
+        Console.WriteLine("Starting task 2.");
+        StringBuilder y = new StringBuilder("Hello");
+        y.AppendLine("Jeruk");
+        for (int i = 0; i < 1000000; i++)
+        {
+            y.Append(i).Append(" ");
+        }
+        Console.WriteLine("Task 2 complete.");
+    }
+}
