5c107bcb8c5f97eac7d9c2a33d826ece8fb4d95a
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-06-12 16:52:46 +0700

    masih error

--- 07_MethodTask/StaticClass.cs
+++ 07_MethodTask/StaticClass.cs
@@ -2,13 +2,16 @@ namespace _07_MethodTask;
 using System.Threading;
 using System.Threading.Tasks;
 
-public static async Task ThisAsync(CancellationToken token)
+public class StaticClass
 {
-	for (int i = 0; i <= 10; i++)
+    public static async Task ThisAsync(CancellationToken token)
     {
-        token.ThrowIfCancellationRequested();
+        for (int i = 0; i <= 10; i++)
+        {
+            token.ThrowIfCancellationRequested();
 
-        Console.WriteLine($"Work in progress: {i * 10}%");
-        await Task.Delay(500, token);
+            Console.WriteLine($"Work in progress: {i * 10}%");
+            await Task.Delay(500, token);
+        }
     }
 }
