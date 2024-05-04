1eff3802bcc858ee007e77a99f2e256cd44f76b8
MANGGAPERAS\dell1 <muhammadanggaprasetyo12@gmail.com>
2024-05-04 11:03:53 +0700

    push refactor foobar program

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,36 +5,35 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Dictionary<int, string> rules = new Dictionary<int, string>();
-		rules.Add(3, "foo");
-		rules.Add(5, "bar");
-		rules.Add(7, "fizz");
-		rules.Add(9, "buzz");
+		Dictionary<int, string> rules = new Dictionary<int, string>()
+		{
+			{ 3, "foo" },
+			{ 5, "bar" },
+			{ 7, "fizz" },
+			{ 9, "buzz" }
+		};
 
 		Console.Write("Masukkan angka: ");
 		int n = Convert.ToInt32(Console.ReadLine());
 
-		Console.WriteLine("n = " + n);
+		Console.WriteLine($"n = {n}");
 		Console.Write("Hasil: ");
 
 		for (int i = 1; i <= n; i++)
 		{
-			bool isDivisible = false;
-			string result = "";
-
+			bool divisible = false;
 			foreach (var rule in rules)
 			{
 				if (i % rule.Key == 0)
 				{
-					result += rule.Value;
-					isDivisible = true;
+					Console.Write(rule.Value);
+					divisible = true;
 				}
 			}
+			if (!divisible)
+				Console.Write(i);
 
-			if (!isDivisible)
-				result = i.ToString();
-
-			Console.Write(result + " ");
+			Console.Write(" ");
 		}
 
 		Console.WriteLine();
